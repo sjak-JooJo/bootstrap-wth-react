a26930481fe6d3c068ab8748c4d7af8713d43b2b
JooJo <sjak0101@gmail.com>
2022-08-18 02:12:44 +0100

    added some alter system of bootstrap with react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
-import CardGroup from './components/CardGroup/CardGroup';
+import CardGroup2 from './components/CardGroup2/CardGroup2';
 
 function App() {
   return (
 
     <div className="App">
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
-
-      <button type="button" className="btn btn-link">Link</button>
-      <CardGroup></CardGroup>
+      <h1>Usign Ract Bootstrap</h1>
+      <Button variant="danger">My Button</Button>
+      <br />
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <CardGroup2></CardGroup2>
     </div>
   );
 }
